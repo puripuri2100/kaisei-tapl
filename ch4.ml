3324aa3438007ee9a7043aa4d765c73b80ccbad7
montyaxn <zanka.bgl@gmail.com>
2019-11-10 22:50:03 +0900

    step by step debugging

--- ch4.ml
+++ ch4.ml
@@ -7,6 +7,18 @@ type term =
   | TmPred of term
   | TmIsZero of term
 
+type rule =
+  | E_IfTrue
+  | E_IfFalse
+  | E_If
+  | E_Succ
+  | E_PredSucc
+  | E_PredZero
+  | E_Pred
+  | E_IsZeroZero
+  | E_IsZeroSucc
+  | E_IsZero
+
 
 exception NoRuleApplies
 
@@ -65,21 +77,32 @@ let rec eval t =
 
 
 let rec show t =
-  match t with
+  let rec to_string t_ = match t_ with
   | TmTrue -> "TmTrue"
   | TmFalse -> "TmFalse"
-  | TmIf(t1, t2, t3) -> "TmIf(" ^ show t1 ^ ", " ^ show t2 ^ ", " ^ show t3 ^ ")"
+  | TmIf(t1, t2, t3) -> "TmIf(" ^ to_string t1 ^ ", " ^ to_string t2 ^ ", " ^ to_string t3 ^ ")"
   | TmZero -> "TmZero"
-  | TmSucc(t1) -> "TmSucc(" ^ show t1 ^ ")"
-  | TmPred(t1) -> "TmPred(" ^ show t1 ^ ")"
-  | TmIsZero(t1) -> "TmIsZero(" ^ show t1 ^ ")"
+  | TmSucc(t1) -> "TmSucc(" ^ to_string t1 ^ ")"
+  | TmPred(t1) -> "TmPred(" ^ to_string t1 ^ ")"
+  | TmIsZero(t1) -> "TmIsZero(" ^ to_string t1 ^ ")"
+  in
+  Printf.printf "%s\n" (to_string t)
+
+
+let rec show_step_by_step t =
+  let t'opt =
+    try let t' = eval1 t in Some(t')
+    with NoRuleApplies -> None
+  in
+  match t'opt with
+  | Some(t') -> show t; let _ = read_line () in show_step_by_step t'
+  | None -> show t
 
 
 let main =
   let t = TmIf(TmIsZero(TmSucc(TmZero)), TmSucc(TmZero), TmZero) in
-  Printf.printf "%s\n%s\n%s\n%s\n"
-    (t |> show)
-    (t |> eval1 |> show)
-    (t |> eval1 |> eval1 |> show)
-    (t |> eval |> show)
-
+  (* t |> show;
+  t |> eval1 |> show;
+  t |> eval1 |> eval1 |> show;
+  t |> eval |> show; *)
+  t |> show_step_by_step
